5ca0e179308664f061f4e347cbb663aa6a335227
AnimeLordx <120332363+AnimeLordx@users.noreply.github.com>
2023-10-19 19:36:06 +0300

    Create lab4i.cpp
    Problem I: 74477. Don't wanna be perfect.
    If an element is a perfect square, replace it with its root.
    
    Input format
    The first line contains two integers n (1 <= n <= 100000 ) and m (1 <= m <= 100000) — array size. Remaining lines contain n rows with m integers a[i][j] (1 <= a[i][j] <= 1000000000) — elements of array.
    
    Output format
    Array with some replaced elements.

---
+++ lab4j.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    int m;
+    cin >>n;
+    cin >>m;
+    int array[n][m];
+     for (int i =0; i< n; i++) 
+     {for (int j = 0; j < m; j++){
+            cin >> array[i][j];}
+    } 
+     for (int i =0; i< n; i++) {
+        for (int j = 0; j < m; j++){
+             double root = sqrt(array[i][j]);
+             if (ceil(root) == floor(root)) {
+               cout << static_cast<int>(root) << " ";
+                 } else {
+                cout << array[i][j] << " ";
+             }
+        }cout << endl;
+     }
+    return 0;
+}
