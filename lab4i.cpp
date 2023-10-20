9514d10f31f5a230ce9b45ce9b892b0e115e6bbc
AnimeLordx <120332363+AnimeLordx@users.noreply.github.com>
2023-10-20 13:20:42 +0300

    Update lab4i.cpp

--- lab4i.cpp
+++ lab4i.cpp
@@ -1,25 +1,27 @@
+
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 int main()
 {
-    int n,m;
-    cin >> n >> m;
+    int n;
+    int m;
+    cin >>n;
+    cin >>m;
     int array[n][m];
-     for (int i =0; i< n; i++){
-        for (int j = 0; j < m; j++){
+     for (int i =0; i< n; i++) 
+     {for (int j = 0; j < m; j++){
             cin >> array[i][j];}
     } 
-     for (int i =0; i < n; i++) {
+     for (int i =0; i< n; i++) {
         for (int j = 0; j < m; j++){
-             if( (i+j) % 2 == 0 ){
-                cout << array[i][j]+1 << " ";}
-                else{
-                    cout << array[i][j]-1 << " ";
-                }
-             
-             
+             double root = sqrt(array[i][j]);
+             if (ceil(root) == floor(root)) {
+               cout << static_cast<int>(root) << " ";
+                 } else {
+                cout << array[i][j] << " ";
+             }
         }cout << endl;
      }
     return 0;
