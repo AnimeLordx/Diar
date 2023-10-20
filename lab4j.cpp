10119f07e0d5aac81a8450557cc3dfb31103dd21
AnimeLordx <120332363+AnimeLordx@users.noreply.github.com>
2023-10-20 13:21:37 +0300

    Update lab4j.cpp

--- lab4j.cpp
+++ lab4j.cpp
@@ -4,23 +4,22 @@ using namespace std;
 
 int main()
 {
-    int n;
-    int m;
-    cin >>n;
-    cin >>m;
+    int n,m;
+    cin >> n >> m;
     int array[n][m];
-     for (int i =0; i< n; i++) 
-     {for (int j = 0; j < m; j++){
+     for (int i =0; i< n; i++){
+        for (int j = 0; j < m; j++){
             cin >> array[i][j];}
     } 
-     for (int i =0; i< n; i++) {
+     for (int i =0; i < n; i++) {
         for (int j = 0; j < m; j++){
-             double root = sqrt(array[i][j]);
-             if (ceil(root) == floor(root)) {
-               cout << static_cast<int>(root) << " ";
-                 } else {
-                cout << array[i][j] << " ";
-             }
+             if( (i+j) % 2 == 0 ){
+                cout << array[i][j]+1 << " ";}
+                else{
+                    cout << array[i][j]-1 << " ";
+                }
+             
+             
         }cout << endl;
      }
     return 0;
