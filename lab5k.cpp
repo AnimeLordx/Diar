3ca21c78befc6d691bae3e3f293d7188c4910ace
AnimeLordx <120332363+AnimeLordx@users.noreply.github.com>
2023-10-21 00:53:52 +0300

    Create lab5k.cpp

---
+++ lab5k.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int sum = 0;
+
+    for (int i=0; i < s.length(); i++) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+            sum++;
+        }
+    }
+cout << sum;
+
+    return 0;
+}
