a6b2c950dde664e315153ba9147c8b622eea95f4
AnimeLordx <120332363+AnimeLordx@users.noreply.github.com>
2023-10-21 09:43:32 +0300

    Create lab5c.cpp

---
+++ lab5c.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+string n;
+string m;
+cin >> n;
+cin >> m;
+if(n.find(m) !=-1){
+    cout << "YES";
+}   else{
+    cout <<"NO";
+}
+
+
+    return 0;
+}
